43daae02ab804a3a07feeab4649582e65d6e354b
Alberto <mpalberto96@gmail.com>
2022-06-21 20:01:05 +0200

    Added a slider to select the number of rows and columns

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function createGrid(rowNum = 16) {
 
     for (let i = 1; i <= (rowNum ** 2); i++) {
         let square = document.createElement("div");
+        square.setAttribute('id', 'square');
         square.style.height = `${squareSize}px`;
         square.style.width = `${squareSize}px`;
 
@@ -17,15 +18,16 @@ function createGrid(rowNum = 16) {
         }
 
         gridCont.appendChild(square);
-        colorGrid(square);
+
     }
 
     gridCont.setAttribute('style', `grid-template-columns: repeat(${rowNum}, 1fr)`);
 }
 
-function colorGrid(square) {
+function colorGrid(color = 'blue') {
 
     const gridCont = document.querySelector(".grid-container");
+    let squares = gridCont.childNodes;
 
     let isClicked = false;
 
@@ -37,11 +39,38 @@ function colorGrid(square) {
         isClicked = false;
     })
 
-    square.addEventListener('mouseover', () => {
-        if (isClicked) {
-            square.style.backgroundColor = 'blue';
-        }
+    gridCont.addEventListener('mouseleave', () => {
+        isClicked = false;
     })
+
+    for (let i = 1; i < squares.length; i++) {
+        squares[i].addEventListener('mousemove', () => {
+            if (isClicked) {
+                squares[i].style.backgroundColor = color;
+            }
+        })
+
+        squares[i].addEventListener('click', () => {
+            squares[i].style.backgroundColor = color;
+        })
+    }
 }
 
-createGrid();
+function getGridSize() {
+    let slider = document.getElementById("row-range");
+    let output = document.getElementById("size");
+    output.textContent = slider.value;
+
+    createGrid(slider.value);
+
+    slider.oninput = function() {
+        removeGrid();
+        output.textContent = this.value;
+        createGrid(slider.value);
+      }
+}
+
+function removeGrid() {
+    document.querySelectorAll("#square").forEach((e) => e.parentNode.removeChild(e));;
+}
+
